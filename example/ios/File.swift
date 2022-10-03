a75a8ca0401da3d2efacd50adfc515cfc063628d
do xuan tien <doxuantien@Dos-Mac-mini.local>
2022-10-03 13:52:48 +0700

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  MyLibExample
+//
+
+import Foundation
---
+++ example/react-native.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  dependencies: {
+    'react-native-my-lib': {
+      root: path.join(__dirname, '..'),
+    },
+  },
+};
